dba01ccab5fa74f6c3cf9538ddff56dc96fc3c00
DavePearce <david.pearce@ecs.vuw.ac.nz>
2022-04-22 08:29:39 +1200

    Remove flag for checking
    That's because this is now subsumed into the wy.toml file at this stage.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,3 @@ echo "Running wy build ..."
 echo "================================================================================"
 # Run Whiley build
 wy --verbose build
-# Run Whiley QuickCheck (if requested)
-if [ "$INPUT_CHECKING" = true ] ;
-then
-    echo "================================================================================"
-    echo "Running wy check ..."
-    echo "================================================================================"    
-    wy --verbose check
-fi
